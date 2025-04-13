c7d5d634f86f166c2efd9c67421454360712cbdd
K-Canzater <moneymaking14223@gmail.com>
2025-04-13 19:29:24 -0400

    Format changes

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -8,12 +8,10 @@ module "IAM" {
   iam_group             = "infra-admins"
   iam_policy_attachment = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
 
-
 }
 
 
 module "S3" {
-
 
   source            = "./modules/S3"
   bucket_name       = "cloudinfra-dev-kc"
@@ -29,21 +27,18 @@ module "VPC" {
   availability_zone_subnet = "us-east-1a"
   myip                     = "207.213.57.120/32"
 
-
 }
 
 # EC2
-module "EC2"{
+module "EC2" {
 
-    source = "./modules/EC2"
-    iam_role_name = "ec2-ssm-role"
-    instance_profile_name = "ec2-ssm-profile"
-    ami_id = "ami-00a929b66ed6e0de6"
-    instance_type = "t2.micro"
-    subnet_id = "subnet-0d1c26c4a49adf178"
-    security_group_id = "sg-00913c4ea83601f21"
-    ec2_name = "dev-ec2-instance"
-
-
+  source                = "./modules/EC2"
+  iam_role_name         = "ec2-ssm-role"
+  instance_profile_name = "ec2-ssm-profile"
+  ami_id                = "ami-00a929b66ed6e0de6"
+  instance_type         = "t2.micro"
+  subnet_id             = "subnet-0d1c26c4a49adf178"
+  security_group_id     = "sg-00913c4ea83601f21"
+  ec2_name              = "dev-ec2-instance"
 
 }
--- Terraform/providers.tf
+++ Terraform/providers.tf
@@ -2,15 +2,15 @@
 terraform {
   required_providers {
     aws = {
-        source = "hashicorp/aws"
-        version = "~>5.0"
+      source  = "hashicorp/aws"
+      version = "~>5.0"
     }
   }
 }
 
 
 provider "aws" {
-    region = var.aws_region
+  region = var.aws_region
 
-  
+
 }
