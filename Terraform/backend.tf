bf6c2542941ca610e9af303415dc5728c275a058
K-Canzater <moneymaking14223@gmail.com>
2025-04-13 16:08:26 -0400

    Set up Root files in Project

---
+++ Terraform/backend.tf
@@ -0,0 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket = "k-canzater-terraform-state"
+    key = "terraform/aws-infra-automation.tfstate"
+    region = "us-east-1"
+    encrypt = true
+    dynamodb_table = "terraform-locks"
+    kms_key_id = "arn:aws:kms:us-east-1:586794467243:key/c9943898-a5c5-4f41-8c05-687088f1b181"
+    
+  }
+}
---
+++ Terraform/variables.tf
@@ -0,0 +1,6 @@
+variable "aws_region" {
+    description = "The Region we will be setting resources up in"
+    type = string
+    default = "us-east-1"
+  
+}
